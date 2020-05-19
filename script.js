3ba9dc73b57adc9bd6ada6f6fc61bdefdb4f7f92
Igor Boitsev <igor.boitsev@gmail.com>
2020-05-19 11:13:22 +0300

    burgerMenu

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ const burgerMenu = () => {
 }
 
 burgerMenu();
+
+// Выбор клуба из падающего меню
+const chooseClubBtn = () => {
+  const clubsList = document.querySelector('.clubs-list'),
+        clubsListUl = clubsList.querySelector('ul');
+
+  clubsList.addEventListener('click', () => {
+    if (clubsListUl.style.display === '' || clubsListUl.style.display === 'none') {
+      clubsListUl.style.display = 'block';
+    } else {
+      clubsListUl.style.display = 'none';
+    }
+  });
+}
+
+chooseClubBtn();
